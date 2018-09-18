edbd3d1d319412588c5cf8df9a3a24d2ad4f26f6
nishida1 <nishidanexus6@gmail.com>
2018-09-18 16:26:45 +0900

    READ_EXTERNAL_STORAGE

--- app/src/main/java/com/example/nishida/imagetest/MainActivity.java
+++ app/src/main/java/com/example/nishida/imagetest/MainActivity.java
@@ -1,13 +1,149 @@
 package com.example.nishida.imagetest;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private ImageView imageView;
+
+    private final int REQUEST_PERMISSION = 1000;
+
+    // asset の画像ファイル名
+    private String fileName = "sample_image.jpg";
+
+    private String filePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.text_view);
+        String str = "image file: "+fileName;
+        textView.setText(str);
+
+        imageView = findViewById(R.id.image_view);
+
+        // Android 6, API 23以上でパーミッシンの確認
+        if(Build.VERSION.SDK_INT >= 23){
+            checkPermission();
+        }
+        else{
+            setUpWriteExternalStorage();
+        }
+    }
+
+    private void setUpWriteExternalStorage(){
+
+        // 画像を置く外部ストレージのパスを設定
+        filePath = Environment.getExternalStorageDirectory().getPath()
+                + "/DCIM/Camera/"+fileName;
+        //Log.d("debug","filePath="+filePath);
+
+
+
+        // TODO 画像読み込み処理
+        Button buttonRead = findViewById(R.id.button_read);
+        buttonRead.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View arg0) {
+
+                //File file = new File(filePath);
+
+                File file = new File(Environment.getExternalStorageDirectory()+"/test.jpg");
+
+                try(InputStream inputStream0 =
+                            new FileInputStream(file); ) {
+
+                    Bitmap bitmap = BitmapFactory.decodeStream(inputStream0);
+                    imageView.setImageBitmap(bitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            };
+        });
+
+    }
+
+    // アンドロイドのデータベースへ登録する
+    private void registerDatabase(String file) {
+        ContentValues contentValues = new ContentValues();
+        ContentResolver contentResolver = MainActivity.this.getContentResolver();
+        contentValues.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+        contentValues.put("_data", file);
+        contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                contentValues);
+    }
+
+    // permissionの確認
+    public void checkPermission() {
+        // 既に許可している
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_GRANTED){
+            setUpWriteExternalStorage();
+        }
+        // 拒否していた場合
+        else{
+            requestLocationPermission();
+        }
+    }
+
+    // 許可を求める
+    private void requestLocationPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_PERMISSION);
+        } else {
+            Toast toast = Toast.makeText(this, "許可してください", Toast.LENGTH_SHORT);
+            toast.show();
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,},
+                    REQUEST_PERMISSION);
+        }
+    }
+
+    // 結果の受け取り
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_PERMISSION) {
+            // 使用が許可された
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                setUpWriteExternalStorage();
+            } else {
+                // それでも拒否された時の対応
+                Toast toast = Toast.makeText(this, "何もできません", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        }
     }
 }
